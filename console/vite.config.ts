026f622ff86dd3b9c023a711e650e2baa6a02603
Pierre Bazoge <pierre@bazoge.com>
2025-05-08 22:05:41 +0200

    build

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -33,7 +33,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
-    }
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   },
   optimizeDeps: {
     include: ['@fortawesome/react-fontawesome', '@fortawesome/fontawesome-svg-core']
