fa765489f79ac219265a4676d38256c676476dd0
Pierre Bazoge <pierre@bazoge.com>
2025-05-26 14:28:26 +0200

    home

--- homepage/tailwind.config.js
+++ homepage/tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        '2xl': '1078px',
+      },
+    },
     extend: {
       colors: {
         primary: '#7763F1',
